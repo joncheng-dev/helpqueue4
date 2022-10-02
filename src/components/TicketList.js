b42c2c2211504290525397c6518a6d813098311d
Jonathan Cheng <jcheng461@gmail.com>
2022-10-02 15:22:21 +0900

    ticketList component loops through array and uses Ticket component to display to screen

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -2,10 +2,30 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList() {
+  const mainTicketList = [
+    {
+      names: "Bart and Lisa",
+      location: "Springfield, PA",
+      issue: "Fireworks stuck on the roof!",
+    },
+    {
+      names: "Abe Simpson",
+      location: "Simpson residence",
+      issue: "Cat ran away!",
+    },
+    {
+      names: "Patty and Selma",
+      location: "Moe's Tavern",
+      issue: "New no smoking law",
+    },
+  ];
+
   return (
     <React.Fragment>
-      <Ticket names="Bart and Lisa" location="Springfield, PA" issue="Fireworks don't work." />
-      <Ticket names="Abe Simpson" location="Simpson residence" issue="Cat ran out the house." />
+      {mainTicketList.map((ticket, index) => (
+        <Ticket names={ticket.names} location={ticket.location} issue={ticket.issue} key={index} />
+      ))}
+      ;
     </React.Fragment>
   );
 }
